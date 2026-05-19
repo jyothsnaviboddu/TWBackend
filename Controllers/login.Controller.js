a796eb49342a7ce95db745681c8079c37710e309
jyothsnaviboddu <jyothsnaviboddu@gmail.com>
2026-05-19 20:45:38 +0530

    login

--- Controllers/login.Controller.js
+++ Controllers/login.Controller.js
@@ -4,9 +4,9 @@ const jwt=require("jsonwebtoken")
 const login=async (req,res)=>
 {
     console.log(req.body)
-    var users=[]
     if(req.body.role==="user")
-     {
+     { 
+        let users=[]
         console.log("User role....")
          await patientmodel.find({"role":req.body.role}).then((data)=>
         {
@@ -35,32 +35,8 @@ const login=async (req,res)=>
    }
  else if(req.body.role==="admin")
      {
-        console.log("User role....")
-         await usersModel.find({"role":req.body.role}).then((data)=>
-        {
-            users=data
-        })
-        const a_u=users.find((user)=>
-        {
-            
-                if(user.username===req.body.username && user.password===req.body.password)
-                {
-                    return true;
-                }
-         
-        })
-        console.log(a_u)
-        if(a_u)
-        {
-                        var  token=jwt.sign({...req.body},"Hello User")
-                        res.send({msg:"login success",token,username:req.body.username,role:req.body.role})
-
-        }  
-        else
-        {
-            res.send({msg:"credentials mismatched"})
-        }     
-   }
+            res.send({"msg":"hello"})
+     }
    else
    {
             res.send("Credentials mismatched")
