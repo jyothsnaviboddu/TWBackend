84d4d0a4e4a4220f3bf844beaae9c506d4343933
jyothsnaviboddu <jyothsnaviboddu@gmail.com>
2026-05-20 10:50:27 +0530

    patient

--- Controllers/patient.Controllers.js
+++ Controllers/patient.Controllers.js
@@ -4,7 +4,9 @@ const patient= async (req,res)=>
      var newPatient=new patientmodel(req.body)
     await newPatient.save()
      console.log(req.body)
-   await res.send({"msg":"patient added",newPatient})
+  // await res.send({"msg":"patient added",newPatient})
+  res.status(201).json({ newPatient });
+
 }
 const allPatients= (req,res)=>{ 
      patientmodel.find().then((data)=>{
